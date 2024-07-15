ed20ba452cab56a8dc49f32934803ef68e7bb996
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-16 01:05:11 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,6 +9,8 @@
 // Function declarations
 int add(const char *input);
 char parse_delimiter(const char *input);
+char parse_custom_delimiter(const char *input);
+char parse_default_delimiter(const char *input);
 void parse_numbers(const char *input, char delimiter, int *sum);
 void handle_negatives(const char *input);
 
@@ -27,23 +29,32 @@ int add(const char *input) {
 
 // Function to parse the delimiter from input string
 char parse_delimiter(const char *input) {
-    char delimiter = ',';
     if (strncmp(input, "//", 2) == 0) {
-        const char *delimiter_start = strchr(input, '[');
-        if (delimiter_start != NULL) {
-            const char *delimiter_end = strchr(delimiter_start + 1, ']');
-            if (delimiter_end != NULL) {
-                int delimiter_length = delimiter_end - delimiter_start - 1;
-                char custom_delimiter[delimiter_length + 1];
-                strncpy(custom_delimiter, delimiter_start + 1, delimiter_length);
-                custom_delimiter[delimiter_length] = '\0';
-                delimiter = custom_delimiter[0];
-            }
-        } else {
-            delimiter = input[2];
+        return parse_custom_delimiter(input);
+    } else {
+        return parse_default_delimiter(input);
+    }
+}
+
+// Function to parse custom delimiter from input string
+char parse_custom_delimiter(const char *input) {
+    const char *delimiter_start = strchr(input, '[');
+    if (delimiter_start != NULL) {
+        const char *delimiter_end = strchr(delimiter_start + 1, ']');
+        if (delimiter_end != NULL) {
+            int delimiter_length = delimiter_end - delimiter_start - 1;
+            char custom_delimiter[delimiter_length + 1];
+            strncpy(custom_delimiter, delimiter_start + 1, delimiter_length);
+            custom_delimiter[delimiter_length] = '\0';
+            return custom_delimiter[0];
         }
     }
-    return delimiter;
+    return input[2];
+}
+
+// Function to parse default delimiter from input string
+char parse_default_delimiter(const char *input) {
+    return input[2];
 }
 
 // Function to parse numbers and calculate sum
